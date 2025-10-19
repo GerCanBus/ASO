23f9f69eb07b501fa18156e5edc5cae9e390b805
GerCanBus <gercanbus@alu.edu.gva.es>
2025-10-19 22:45:04 +0200

    Estas son las variables de entorno

---
+++ Practicando/VariablesEntorno.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Este script utiliza el comando 'printenv' para mostrar
+# todas las variables de entorno actuales en la pantalla.
+
+echo "--- Iniciando la impresión de variables de entorno ---"
+
+# Ejecuta el comando
+printenv
+
+echo "--- Impresión de variables de entorno completada ---"
